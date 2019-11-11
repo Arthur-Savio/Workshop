1e07f487f7a2b0e265c13b8661dbb1ddbd3fa4ac
samuel <sls@ic.ufal.br>
2019-11-11 13:36:20 -0300

    models de user deletado

--- ifalflix/models.py
+++ ifalflix/models.py
@@ -3,15 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-
-class User(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField()
-    password = models.CharField(max_length=255)
-    account_type = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.name
+ self.name
     
 
 class Series(models.Model):
